8c656e9b1ac284f4115834f2b7fdc28e481b300e
cattelia <cattelia@gmail.com>
2023-08-01 09:32:09 -0700

    Tuples and Dictionaries
    Working through being able to manipulate and work with Tuples even though they are an immutable object in Python.
    
    And a drop of list comprehensions.

--- practice.py
+++ practice.py
@@ -2,25 +2,81 @@
 #Figure out why Visual Studio is not opening the most recent updates.
 # Vim is what you should be using. Even if it is weird.
 
-#CLEAN Python Input Handling
-def do_this():
-    print("Doing this")
+
+
+
+
+
+#Sorting Tuples in Dictionary
+d = {"a":10, "b":1, "c":22}
+d.items()
+#dict_items([{"a":10, "b":1, "c":22}])
+sorted(d.items())
+#>>> [{"b":1, "a":10, "c":22}]
+
+d = {"a":10, "b":1, "c":22}
+t = sorted(d.items())
+print(t)
+#>>> [{"b":1, "a":10, "c":22}]
+for k, v in sorted(d.items()):
+    print(k,v)
+
+c = {"a":10, "b":1, "c":22}
+#initialize an empty list
+tmp = list()
+for k,v in c.items():
+    #flip the key and value in the list of tuples
+    tmp.append((v, k))
+tmp = sorted(tmp, reverse=True)
+print(tmp)
+
+#Find the most common words in a file
+fhand = open("mobe.txt", "r") #open a file as read only
+counts = dict() #initilize an empty dict
+for line in fhand:
+    words = line.split() #split by spaces
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1 #If no word exists in dict, at it at 1 or +1 on existing words.
     
-def do_that():
-    print("Doing that")
-    
-match input('Do this or that? '):
-    case "this":
-        do_this()
-        
-    case "that":
-        do_that()
-        
-    case _:
-        print("Invalid Input!")
+lst = list()
+for key, val in counts.items(): 
+    newtup = (val, key)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse=True)
+
+for val, key in lst[:10]:
+    print(key, val)
+
+#Do this by a list comprehension. Let's go back to our "c" variable.
+c = {"a":10, "b":1, "c":22}
+print( sorted ( [ (v,k) for k, v in c.items() ] )) #generator for all of the elements
+# [(1, 'b'), (10, 'a'), (22, 'c')]
+#Let's try it without sorted()
+print( [ (v,k) for k, v in c.items() ] )
+# [(10, 'a'), (1, 'b'), (22, 'c')]
+
+#Tuples with Dictionary Tuple Output
+'''
+#Tuples are immutable, which means that they cannot be modified, meaning we can modify slots.
+#CANNOT: .sort(), .append(), .reverse() | CAN: 
+
+a = ("Joseph", "Seth", "Ami", "Margot", "Jerry")
+(x, y) = (4, "Joe")
+
+d = dict()
+d['Joseph'] = 1
+d['Seth'] = 2
+d['Ami'] = 3
+d['Margot'] = 4
+d['Jerry'] = 5
+for (k,i) in d.items():
+    print(k,i)
+'''
 
 
 #Filing Handling and Counting w/ Dictionary
+'''
 counts = dict()
 file = open('mobe.txt', "r")
 for line in file:
@@ -34,6 +90,8 @@ counts = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
 for key in counts:
     if counts[key] > 10:
         print(key, counts[key])
+'''
+
 
 #Dictionary
 '''
@@ -62,6 +120,7 @@ for name in friends:
 print("This is the single for loop for\n {}".format(counts))
 '''
 
+
 #List Handling
 '''
 friends = ["Joseph", "Seth", "Ami", "Margot", "Jerry"]
@@ -81,11 +140,13 @@ b = [1,2,3]
 c = a + b
 '''
 
+
 #Printing on the same line
 '''
 print("Hello ", end="")
 print("{}!".format(friends[0]))
 '''
+
 
 #Filing Handling
 '''
@@ -127,6 +188,7 @@ piece = float(piece)
 #print("{}%".format(piece))
 '''
 
+
 #Decorator Testing
 '''
 from functools import lru_cache
@@ -146,6 +208,7 @@ def increment(number):
 #print("\n")
 '''
 
+
 # *args Testing
 '''
 def check(*list):
@@ -163,4 +226,24 @@ for item in [3,41,12,9,74,15]:
         smallest = item
         print("Loop: ", item, smallest) 
 print("Smallest: ", smallest)
+'''
+
+
+#CLEAN Python Input Handling
+'''
+def do_this():
+    print("Doing this")
+    
+def do_that():
+    print("Doing that")
+    
+match input('Do this or that? '):
+    case "this":
+        do_this()
+        
+    case "that":
+        do_that()
+        
+    case _:
+        print("Invalid Input!")
 '''
