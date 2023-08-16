3e453a4e0c59c2e30c4eb8af93c25a1bc6af97bc
cattelia <cattelia@gmail.com>
2023-08-16 09:41:33 -0700

    Intro to Relational Databases
    This is just an overview of what the use and purpose of a database is as well as the history of the technology.

---
+++ FCC_7/12. relationalDatabases.py
@@ -0,0 +1,35 @@
+#SQL Structured Query Language
+'''
+C reate
+R ead
+U pdate
+D elete
+
+
+I Files -> Python Program -(SQL)-> Db Files
+        /          |                  |
+       /           |                (SQL)
+R    Ofile         |                  |   
+Excel             You    ->     SQLite Browser
+D3.js
+
+
+Web Applications w/ Databases
+- Application Developer  - builds the logic for the application, the look and feel of the application - monitors for problems
+- Database Administrator - Monitors and adjusts the database as the program runs in production
+- Often both people participate in the building of the "Data Model"
+
+End User => Application Software -> Database Data Software -> Database Tools
+                    (Dev)                                          (DBA)
+
+Common Database Systems:
+- Oracle
+- MySQL
+- SqlServer
+- HSQL, SQLite (embedded database system), Postgres
+'''
+
+#Not much going on. Next we will be working directly in SQLite.
+
+
+
