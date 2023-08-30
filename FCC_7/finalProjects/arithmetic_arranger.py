e797999bd4cbd415e164ea38500f3f1db75ad95c
cattelia <cattelia@gmail.com>
2023-08-30 08:37:00 -0700

    Final Project Continued
    Included Error messages for:
            1- 5> errors
            2- only "+" and "-".
            3- only numbers in the.. numbers.
            4- each number has max 4 digits
    
    Starting on formatting output.

--- FCC_7/finalProjects/arithmetic_arranger.py
+++ FCC_7/finalProjects/arithmetic_arranger.py
@@ -1,47 +1,73 @@
-#from multiprocessing import Pool
+#def arithmetic_arranger(problems, values=False):
+'''
+    Errors:
+        1- 5> errors, break. "Error: Too many problems."
+        2- only "+" and "-". "Error: Operator must be '+' or '-'."
+        3- only numbers in the.. numbers. "Error: Numbers must only contain digits."
+        4- each number has max 4 digits "Error: Numbers cannot be more than four digits."
+'''
 
 list = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-number1 = []
+values = False  #optional
+left = []
 operator = []
-number2 = []
-answer = []
-#count how many errors fed to the function. Should return error at 5.
-errors = 0
+right = []
+answer = []  #optional if values = True
 
 #get items in their own lists
 for line in list:
-    newlist = line.split()
 
-    #get answers
-    if newlist[1] == "+":
-        answer.append(int(newlist[0]) + int(newlist[2]))
-    elif newlist[1] == "-":
-        answer.append(int(newlist[0]) - int(newlist[2]))
+    #check to make sure there are <= 5 entered problems
+    if len(list) > 5:
+        print("Error: Too many problems.")
+        break
+
+    newlist = line.split()  #>>> ["32", "+", "698"]
+
+    #check is both left and right operand are only numbers
+    if newlist[0].isnumeric() == False or newlist[2].isnumeric() == False:
+        print("Error: Numbers must only contain digits.")
+        break
     else:
-        #error message to user showing it cannot solve * or /
-        print("Error: Operator must be '+' or '-'.")
-        errors += 1
-        if errors == 5:
-            print("Error: Too many problems.")
+        #check to make sure no number is greater than 4 digits
+        if len(newlist[0]) > 4 or len(newlist[2]) > 4:
+            print("Error: Numbers cannot be more than four digits.")
             break
-
-
-    number1.append(newlist[0])
-    operator.append(newlist[1])
-    number2.append(newlist[2])
-
-
-
-
-print(number1, operator, number2, answer)
-
-
-'''
-        if i == "+":
-            answer.append(int(i[0]) + int(i[2]))
-        elif i == "-":
-            answer.append(int(i[0]) - int(i[2]))
         else:
-            print("I cannot solve this operator.")
+            left.append(newlist[0])
+            right.append(newlist[2])
 
+    #check that we are only supplying addition or subtraction
+    if newlist[1] == "+" or newlist[1] == "-":
+        operator.append(newlist[1])
+        #get answers
+        if newlist[1] == "+":
+            answer.append(int(newlist[0]) + int(newlist[2]))
+        elif newlist[1] == "-":
+            answer.append(int(newlist[0]) - int(newlist[2]))
+    else:
+        print("Error: Operator must be '+' or '-'.")
+        break
+
+
+#format printing
 '''
+Left: ['32', '3801', '45', '123']
+Operator: ['+', '-', '+', '+']
+Right: ['698', '2', '43', '49']
+Answers: [730, 3799, 88, 172] *optional
+
+   32      3801      45      123              Left
++ 698    -    2    + 43    +  49    Operator Right
+-----    ------    ----    -----    Line------Line (4 spaces)  
+'''
+
+for i in left:
+    print("{:>}".format(i), end="    ")
+
+#sanity check
+#print("Left Number: {}\nOperator: {}\nRight Number: {}\nAnswers: {}".format(left, operator, right, answer))
+
+#arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+# Run unit tests automatically
+#main(['-vv'])
