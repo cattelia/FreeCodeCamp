44696d3de8b6727e1cbcfc772a8bdb7153838264
cattelia <cattelia@gmail.com>
2023-10-02 09:54:42 -0700

    Setup Main Functions & Testing OOP
    Understanding scope of variable and declaration override.

--- FCC_7/finalProjects/Budget App/budget.py
+++ FCC_7/finalProjects/Budget App/budget.py
@@ -5,25 +5,37 @@ withdraw_ledger = {}
 
 class Bank:
 
+
     def __init__(self) -> None:
-        print("I have made a bank account")
+        print("I have made a new bank account")
 
-    def deposit(self, amount, description):
+    def deposit(self, amount, description=""):
         pass
 
-    def withdraw():
+    def withdraw(self, amount, description=""):
         pass
 
-    def get_balance():
+    def get_balance(self):
         pass
 
-    def create_spend_chart(categories):
-        '''
-        In: List of categories
-        Out: ASCII Chart representing how much was spent in each category
-        '''
-        for i in range (100, -10, -10):
-            print("{:>3}|".format(i))
+    def transfer(self):
         pass
+
+    def check_funds(self):
+        pass
+
+
+def create_spend_chart(categories):
+    '''
+    In: List of categories
+    Out: ASCII Chart representing how much was spent in each category based on percentage
+    '''
+    for i in range (100, -10, -10):
+        print("{:>3}|".format(i))
+    pass
 
 food = Bank()
+food.withdraw(100)
+food.withdraw(12)
+entertainment = Bank()
+entertainment.withdraw(1000)
