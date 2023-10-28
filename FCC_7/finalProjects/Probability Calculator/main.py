a2a768181bf334c6dea172f9fc4ca6cf62dfb1ba
cattelia <cattelia@gmail.com>
2023-10-28 09:22:43 -0700

    Update main.py

--- FCC_7/finalProjects/Probability Calculator/main.py
+++ FCC_7/finalProjects/Probability Calculator/main.py
@@ -1,6 +1,6 @@
 import prob_calculator
 
-'''
+
 hat = prob_calculator.Hat(red=3,blue=2)
 actual = hat.contents
 print(actual)
@@ -14,7 +14,7 @@ print(actual)
 actual = len(hat.contents)
 print(actual)
 #expected = 5 PASSED
-'''
+
 hat = prob_calculator.Hat(yellow=5,red=1,green=3,blue=9,test=1)
 probability = prob_calculator.experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
 actual = probability
