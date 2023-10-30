5a15afc5fa4623ab897ff9f28947bab5b00ef74e
cattelia <cattelia@gmail.com>
2023-10-30 08:54:48 -0700

    Setup

---
+++ FCC_8/Studies/Intro to Data.py
@@ -0,0 +1,6 @@
+'''
+What is Data Analysis?
+    A process of inspecting, cleaning, transforming, and modeling data with the goal of discovering useful information, informing conclusion and supporting decision-making.
+
+
+'''
