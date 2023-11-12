e55c7d304cfcc892a88887f18b5b5941f4968e89
cattelia <cattelia@gmail.com>
2023-11-12 09:44:42 -0800

    Resources
    Getting Jupyter/Conda/VS Code/Python/Windows all working together.

---
+++ FCC_8/Studies/Intro to Data.py
@@ -0,0 +1,44 @@
+'''
+What is Data Analysis?
+    A process of inspecting, cleaning, transforming, and modeling data with the goal of discovering useful information, informing conclusion and supporting decision-making.
+
+Think:
+    Pandas, Matplotlib, and Numpy
+'''
+
+'''
+
+Data Analysis Tools:
+Software:
+    qlik
+    Tableau
+    Looker
+    ZoHo Analytics
+
+Programming Langs:
+    Python
+    R
+    Julia
+
+'''
+
+'''
+
+When to choose R?
+    When R Studio is needed
+    When dealing with advanced stats methods
+    When extreme performance is needed
+
+'''
+
+'''
+Data Extraction:
+    SQL, scrapping, Files, Consulting APIs, Bought Data, etc.
+Data Cleaning:
+    Missing, clearing empty cells, incorrect types, outliers, non relevant info, stat sanitization, etc.
+Data Wrangling:
+    Hierach Data, Categorical, Reshape/Transform, Merge/Combine, Indexing, etc.
+Analysis:
+    Exploration, Building stat models, Visuals and representations, Reporting, stat analysis, hypothesis testing, etc
+
+'''
