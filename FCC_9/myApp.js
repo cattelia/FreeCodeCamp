bf5cc7653dc59f54541854fbd2b34ec91e023e04
cattelia <cattelia@gmail.com>
2024-03-11 21:01:00 -0700

    Update myApp.js

--- FCC_9/myApp.js
+++ FCC_9/myApp.js
@@ -1,55 +1,57 @@
 const express = require('express');
+
+// My code
+var timeInSeconds = 90*24*60*60;
+const helmet = require("helmet")
+// End My code
+
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+
+// My code
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({
+  action: 'deny'
+}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+
+app.use(helmet.hsts({
+  maxAge: timeInSeconds, 
+  force: true
+}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+
+//https://cybeready.com/content-security-policy/helmet-content-security-policy
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com'],
+  }
+}));
+// End My code
+
+// My code : implement helmet Parent
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
+// End My code
+
+
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
