7d64ba5b8a9892a4a66067f15e75d80fc07e8212
cattelia <cattelia@gmail.com>
2024-04-11 13:25:49 -0700

    functionality

--- FCC_9/Penetration-Testing/StockPriceChecker.py
+++ FCC_9/Penetration-Testing/StockPriceChecker.py
@@ -1,11 +1,53 @@
-name = 0
-age = "Sara"
+# YcOZPNhykQqAE2Kb DB password
+# cattelia : YcOZPNhykQqAE2Kb
+# URI Link: "mongodb+srv://cattelia:YcOZPNhykQqAE2Kb@cluster.xqwz3kx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
+# mongodb+srv://cattelia:<password>@cluster.xqwz3kx.mongodb.net/
+# mongodb+srv://cattelia:YcOZPNhykQqAE2Kb@cluster.xqwz3kx.mongodb.net/
+# 24/04/10: 76.146.226.58/32
 
-def test_type(name: str, age: int):
-    print(name, age)
-    name = name
-    print(name, age)
-    age = age
-    print(name, age)
-   
-test_type(name, age)         
+'''
+
+
+api.js>>>
+
+  app.use(helmet({
+    frameguard: {
+      action: "deny"
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["style.css"],
+      }
+    }
+  }))
+  
+  
+  
+  '''
+
+module.exports = function (app) {
+// CSP to only allow scripts and CSS
+
+app.use(helmt.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'", "style.css"],
+  }
+}))
+
+
+app.use(helmet({
+  frameguard: {
+    action: "deny"
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["style.css"],
+    }
+  }
+}))
